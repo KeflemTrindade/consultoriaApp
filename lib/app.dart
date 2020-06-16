0393fdec82561bb74ac9f19692f19e21ac2a9188
Keflem trindade <keflemtn@gmail.com>
2020-06-15 20:44:59 -0400

    ajustes tela inicial

--- lib/app.dart
+++ lib/app.dart
@@ -6,37 +6,56 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  void _abrirEmpresa() {}
+
+  void _abrirServicos() {}
+
+  void _abrirClientes() {}
+
+  void _abrirContato() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
           title: Text("ATI Consultoria"), backgroundColor: Colors.purple),
       body: Container(
-        padding: EdgeInsets.only(top: 50),
+        padding: EdgeInsets.all(16),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image.asset("images/logo.png"),
             Padding(
               padding: EdgeInsets.only(top: 32),
-              child: Image.asset("images/logo.png"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 16),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Image.asset("images/menu_empresa.png"),
-                  Image.asset("images/menu_servico.png")
+                  GestureDetector(
+                    onTap: _abrirEmpresa,
+                    child: Image.asset("images/menu_empresa.png"),
+                  ),
+                  GestureDetector(
+                    onTap: _abrirServicos,
+                    child: Image.asset("images/menu_servico.png"),
+                  )
                 ],
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(bottom: 16),
+              padding: EdgeInsets.only(top: 32),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Image.asset("images/menu_cliente.png"),
-                  Image.asset("images/menu_contato.png")
+                  GestureDetector(
+                    onTap: _abrirClientes,
+                    child: Image.asset("images/menu_cliente.png"),
+                  ),
+                  GestureDetector(
+                    onTap: _abrirContato,
+                    child: Image.asset("images/menu_contato.png"),
+                  )
                 ],
               ),
             )
